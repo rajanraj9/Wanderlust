dbd70d1cdf2d2060634db3664380c504cf493219
Rajan Kumar <rajan847104@gmail.com>
2026-01-31 11:39:41 +0530

    add and delete review rating

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.get("/listings/new", (req, res) => {
 //show Route
 app.get("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("./listings/show.ejs", { listing });
 }));
 
@@ -104,7 +104,7 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
 }));
 
 //Review
-//post Route
+//Post Review Route
 
 app.post("/listings/:id/reviews",validateReview,wrapAsync( async (req, res) => {
     let listing = await Listing.findById(req.params.id);
@@ -118,6 +118,18 @@ app.post("/listings/:id/reviews",validateReview,wrapAsync( async (req, res) => {
     res.redirect(`/listings/${listing._id}`);
 
 }));
+
+//Delete Review Route
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
+    let {id, reviewId} = req.params;
+
+    await Listing.findByIdAndUpdate(id, {$pull: {reviews : reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+
+}
+));
 
 app.use((req, res, next) => {
     next(new ExpressError(404, "Page Not Found"));
--- models/listings.js
+++ models/listings.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
+const review = require("./review");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
     title: {
@@ -36,6 +38,14 @@ const listingSchema = new Schema({
       ref: "Review",
     },
   ],
+});
+
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if(listing){
+    await Review.deleteMany({_id : {$in : listing.reviews}});
+
+  }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
